b622027f43692e16a5b747190c933f167ffc6194
MohmmedElshiwy <mohammed6elshiwy@gmail.com>
2025-06-14 23:32:27 +0300

    add Geolocation

--- components/js/index.js
+++ components/js/index.js
@@ -1,48 +1,77 @@
+window.onload = function () {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
+        getApi(`${lat},${lon}`);
+      },
+      (error) => {
+        console.log("فشل في تحديد الموقع:", error.message);
+        getApi("cairo"); 
+      }
+    );
+  } else {
+    console.log("المتصفح لا يدعم تحديد الموقع");
+    getApi("cairo"); 
+  }
+};
+
 async function getApi(city = "cairo") {
   try {
-    let res = await fetch(
+    let res1 = await fetch(
       `https://api.weatherapi.com/v1/forecast.json?key=dc6b0e058e73400d816182605251306&q=${city}&days=3&aqi=no&alerts=no`
     );
-    let data = await res.json();
-    console.log(data);
+    let data1 = await res1.json();
 
-    displayWather(data);
+    let res2 = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=dc6b0e058e73400d816182605251306&q=${city}&aqi=no`
+    );
+    let data2 = await res2.json();
+
+    displayWeather(data1, data2);
   } catch (error) {
-    console.log("فشل في جلب البيانات:", error);
     document.querySelector(".now").innerHTML = `<h3>فشل في تحميل الطقس</h3>`;
   }
 }
 
-getApi();
-
-function displayWather(data) {
+function displayWeather(data, data2) {
   let forecastDays = data.forecast.forecastday;
+  let cityName = data.location.name;
+  let currentWeather = data2.current.temp_c;
 
-  let cardsHTML = forecastDays
-    .map((day) => {
+  let cardsHTML = `<h2 class="text-center mb-4">${cityName}</h2><div class="row justify-content-center">`;
+  cardsHTML += forecastDays
+    .map((day,index) => {
       let date = new Date(day.date);
-      let weekday = date.toLocaleDateString("en", { weekday: "long" }); //مغشوشة هههه
+      let weekday = date.toLocaleDateString("en", { weekday: "long" });
       let fullDate = date.toLocaleDateString();
       let condition = day.day.condition.text;
       let icon = day.day.condition.icon;
-      let max = day.day.maxtemp_c;
-      let min = day.day.mintemp_c;
+      let max = Math.floor(day.day.maxtemp_c);
+      let min = Math.floor(day.day.mintemp_c);
 
       return `
-          <div class="card col-md-3 p-3 text-center">
-            <h4>${weekday}</h4>
-            <p>${fullDate}</p>
-            <img src="https:${icon}" alt="${condition}" width="100" />
-            <h5 class="my-2">${condition}</h5>
-            <p>🌡️ ${max}° / ${min}°</p>
-          </div>
-        `;
+        <div class="card col-md-4 gap-4 p-3 text-center">
+          <h4>${weekday}</h4>
+          <p>${fullDate}</p>
+          <img src="https:${icon}" alt="${condition}" width="100" />
+  ${
+            index === 0
+              ? `<h3>${currentWeather}°</h3>` 
+              : ""
+          }          <h5 class="my-2">${condition}</h5>
+          <p>🌡️ ${max}° / ${min}°</p>
+        </div>
+      `;
     })
     .join("");
+
   document.querySelector(".now").innerHTML = cardsHTML;
 }
 
-document.getElementById("searchBtn").addEventListener("click", () => {
+document.getElementById("cityInput").addEventListener("input", () => {
   let city = document.getElementById("cityInput").value.trim();
   if (city !== "") {
     getApi(city);
